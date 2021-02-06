73e611cd776517e5a68dede6ec07375815fa96cf
Bruno Shiroma <brunoshiroma@gmail.com>
2021-02-05 21:43:10 -0300

    initial commit

---
+++ xamarin-dex/xamarin-dex/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using xamarin_dex.ViewModels;
+using xamarin_dex.Views;
+
+namespace xamarin_dex
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ xamarin-dex/xamarin-dex/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using xamarin_dex.Models;
+using xamarin_dex.ViewModels;
+
+namespace xamarin_dex.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
